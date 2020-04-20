960aea91efa5421e9f3b358e4e5edb3576bf86e1
QuokeCola <quokecola@icloud.com>
2020-04-20 18:59:52 +0800

    Initial Commit

---
+++ SimuLoc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimuLoc
+//
+//  Created by 钱晨 on 2020/4/20.
+//  Copyright © 2020年 钱晨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
